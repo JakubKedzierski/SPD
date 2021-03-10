a9d6e6cfa5fe3a7303239581457bad17b360b7e8
JakubKedzierski <248915@student.pwr.edu.pl>
2021-03-10 22:11:03 +0100

    comments done

--- lab2_flowshop1/main.py
+++ lab2_flowshop1/main.py
@@ -1,5 +1,3 @@
-import sys
-
 
 def read_data_set(file):
     file.readline()
@@ -17,19 +15,48 @@ def read_data_set(file):
 
     return tasks,machines,time_matrix,Cmax,schedule
 
+def draw_gantt(schedule,time_matrix):
+    pass
+
+
+
+def total_review(tasks,machines,time_matrix):
+    pass
+    # return schedule, Cmax
+
+
+def johnson_for_2_machines(tasks,machines,time_matrix):
+    pass
+    # return schedule, Cmax
+
+
+def johnson_for_N_machines(tasks,machines,time_matrix):
+    pass
+    # return schedule, Cmax
+
 
 
 def main():
     path=""
     file_name="data.txt"
-    number_of_datasets_to_read=1
+    number_of_datasets_to_read=1   # liczba setów, jakie mają zostac odczytane z pliku - mozemy na poczatku pracowac na tym pierwszym poczatkowym
 
     try:
         with open(path+file_name, "r") as file:
             for i in range(0,number_of_datasets_to_read):
                 tasks,machines,time_matrix,Cmax,schedule=read_data_set(file)
+
+                """
+                Cmax i schedule - sa to wyniki z danego datasetu, mozemy je uzyc gdzies pozniej do testowania danych
+                na przyklad w taki sposób:
+                """
+
+                """
+                 our_schedule, our_Cmax = johnson_for_2_machines
+                 if (our_schedule == schedule and our_Cmax = Cmax) good_result_count++
+                """
                 
-                print(tasks,machines,"\n",time_matrix,"\n",Cmax,"\n",schedule)
+                # print(tasks,machines,"\n",time_matrix,"\n",Cmax,"\n",schedule)
             
 
     except FileNotFoundError:
