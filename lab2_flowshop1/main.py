444a2a5f250eb89ec95fb8e59a94407af20140a8
ptroszcz <61745721+ptroszcz@users.noreply.github.com>
2021-03-12 13:55:48 +0100

    Add draw_gannt function

--- lab2_flowshop1/main.py
+++ lab2_flowshop1/main.py
@@ -1,12 +1,16 @@
+import plotly.express as px
+import pandas as pd
+#pip3 install plotly==4.14.3
+#pip install pandas
 
 def read_data_set(file):
-    file.readline()
+    #file.readline()
     tasks, machines = [int(x) for x in next(file).split()] 
     time_matrix = [] 
     
     for i in range(0,tasks):
         time_matrix.append([int(x) for x in next(file).split()])
-        
+    '''    
     file.readline()
     file.readline()
     Cmax = int(file.readline())
@@ -14,8 +18,40 @@ def read_data_set(file):
     file.readline()
 
     return tasks,machines,time_matrix,Cmax,schedule
+    '''
+    return tasks, machines, time_matrix
 
 def draw_gantt(schedule,time_matrix):
+    if len(time_matrix) != len(schedule):
+        raise ValueError('Invalid sequnce number')
+    tasksnr=len(time_matrix)
+    machinesnr=len(time_matrix[0])
+    m='Machine nr '
+    task='Task nr '
+    Cmatrix=[[0 for x in range(machinesnr)] for y in range(tasksnr)] #macierz czasow zakonczen poszczegolnych zadan na danej maszynie
+    for i in range(0,tasksnr):
+        for j in range(0,machinesnr):
+            if i==0:
+                if j==0:
+                    Cmatrix[i][j]=time_matrix[schedule[i]-1][j]
+                    df = pd.DataFrame([dict(Machine=m+str(j+1), Start=0, Finish=Cmatrix[i][j], Task=task+str(schedule[i]))])
+                else:
+                    Cmatrix[i][j]=(Cmatrix[i][j-1]+time_matrix[schedule[i]-1][j])
+                    df=df.append(pd.DataFrame([dict(Machine=m+str(j+1), Start=Cmatrix[i][j-1], Finish=Cmatrix[i][j], Task=task+str(schedule[i]))]))
+            else:
+                if j==0:
+                    Cmatrix[i][j]=Cmatrix[i-1][j]+time_matrix[schedule[i]-1][j]
+                    df=df.append(pd.DataFrame([dict(Machine=m+str(j+1), Start=Cmatrix[i-1][j], Finish=Cmatrix[i][j], Task=task+str(schedule[i]))]))
+                else:
+                    Cmatrix[i][j]=max(Cmatrix[i-1][j],Cmatrix[i][j-1])+time_matrix[schedule[i]-1][j]
+                    df=df.append(pd.DataFrame([dict(Machine=m+str(j+1), Start=max(Cmatrix[i-1][j],Cmatrix[i][j-1]), Finish=Cmatrix[i][j], Task=task+str(schedule[i]))]))
+    fig = px.timeline(df, x_start="Start", x_end="Finish", y="Machine", color="Task")
+    fig.update_yaxes(autorange="reversed")
+    fig.layout.xaxis.type = 'linear'
+    for i in range(0,tasksnr):
+        fig.data[i].x = time_matrix[schedule[i]-1]
+    fig.show()
+
     pass
 
 
@@ -38,14 +74,14 @@ def johnson_for_N_machines(tasks,machines,time_matrix):
 
 def main():
     path=""
-    file_name="data.txt"
+    file_name="data0.txt"
     number_of_datasets_to_read=1   # liczba setów, jakie mają zostac odczytane z pliku - mozemy na poczatku pracowac na tym pierwszym poczatkowym
 
     try:
         with open(path+file_name, "r") as file:
             for i in range(0,number_of_datasets_to_read):
-                tasks,machines,time_matrix,Cmax,schedule=read_data_set(file)
-
+                ##tasks,machines,time_matrix,Cmax,schedule=read_data_set(file)
+                tasks,machines,time_matrix=read_data_set(file)
                 """
                 Cmax i schedule - sa to wyniki z danego datasetu, mozemy je uzyc gdzies pozniej do testowania danych
                 na przyklad w taki sposób:
@@ -55,8 +91,9 @@ def main():
                  our_schedule, our_Cmax = johnson_for_2_machines
                  if (our_schedule == schedule and our_Cmax = Cmax) good_result_count++
                 """
+                draw_gantt([1,4,3,2],time_matrix)
+                #print(tasks,machines,"\n",time_matrix,"\n")
                 
-                # print(tasks,machines,"\n",time_matrix,"\n",Cmax,"\n",schedule)
             
 
     except FileNotFoundError:
