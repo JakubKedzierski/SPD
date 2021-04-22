9b5fefe44499ae42be93da0d117b1ff241512a0b
Jakub Kedzierski <248915@student.pwr.edu.pl>
2021-04-22 15:33:52 +0200

    basic schrage done, tested and works good

--- rpq_problem/schrage_algorithm.py
+++ rpq_problem/schrage_algorithm.py
@@ -10,24 +10,37 @@ def basic_schrage_algorithm(tasks, r, p, q):
     not_ready_list = np.array((not_ready_list, r, p, q))
     not_ready_list = np.transpose(not_ready_list)
     t = np.min(not_ready_list[:, 1])
+    Cmax = 0
 
     while not len(ready_list) == 0 or not len(not_ready_list) == 0:
-        while not len(not_ready_list) == 0 and min(r) <= t:
+        while not len(not_ready_list) == 0 and np.min(not_ready_list[:, 1]) <= t:
             j = np.argmin(not_ready_list[:, 1])
             if len(ready_list) > 0:
                 ready_list = np.vstack((ready_list, not_ready_list[j,:]))
             else:
-                ready_list = not_ready_list[j,:]
+                ready_list = np.zeros((1, 4))
+                ready_list[0, 0] = not_ready_list[j,0]
+                ready_list[0, 1] = not_ready_list[j,1]
+                ready_list[0, 2] = not_ready_list[j,2]
+                ready_list[0, 3] = not_ready_list[j,3]
+
 
             not_ready_list = np.delete(not_ready_list, j, axis= 0)
 
         if len(ready_list) == 0:
             t = np.min(not_ready_list[:, 1])
         else:
-            j = np.argmax(ready_list[:, 3])   # tu blad
+            if i == 0:
+                j = 0
+            else:
+                j = np.argmax(ready_list[:, 3])
+            task = ready_list[j, 0]
+            p_j = ready_list[j, 2]
+            q_j = ready_list[j, 3]
             ready_list = np.delete(ready_list, j, axis=0)
-            schedule[i] = j
+            schedule[i] = task
             i = i+1
-            #t = t+j
+            t = t+p_j
+            Cmax = max(Cmax, t +q_j)
 
-    print(schedule)
+    print(schedule,Cmax)
