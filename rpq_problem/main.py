3412af02dde6bec0581fe78bea329c5eefa37a1e
Jakub Kedzierski <248915@student.pwr.edu.pl>
2021-04-22 12:30:10 +0200

    reading set added

--- rpq_problem/main.py
+++ rpq_problem/main.py
@@ -1,6 +1,31 @@
+def read_data_set(file):
+    tasks, columns = [int(x) for x in next(file).split()]
+
+    r = []
+    p = []
+    q = []
+
+    for i in range(0, tasks):
+        row = next(file).split()
+        r.append(int(row[0]))
+        p.append(int(row[1]))
+        q.append(int(row[2]))
+
+    return tasks, r, p, q
+
 
 def main():
-    pass
+    path=""
+    file_name="./datasets/" + "in50.txt"
+    number_of_datasets_to_read= 1
+
+    try:
+        with open(path + file_name, "r") as file:
+            tasks, r, p, q = read_data_set(file)
+
+    except FileNotFoundError:
+        print("File not found.")
+        raise FileNotFoundError
 
 
 if __name__ == '__main__':
