d8ce0b0fe570ca030f7775a7e76d7eeac94d121f
ptroszcz <61745721+ptroszcz@users.noreply.github.com>
2021-04-23 19:29:42 +0200

    Added priority queue class

---
+++ rpq_problem/piority_queue.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self,r,p,q):
+        self.r=r
+        self.p=p
+        self.q=q
+    
+
+
+class PriorityQueue:
+    def __init__(self,ismax:bool):
+        self.queue=[]
+        self.ismax=ismax
+
+    def isEmpty(self):
+        return (len(self.queue) == 0)
+    
+    def insert(self,node:Node):
+        if (self.ismax):
+            index=len(self.queue)
+            for i in range(0,len(self.queue)):
+                if node.q>self.queue[i].q:
+                    index=i
+                    break
+        else:
+            index=len(self.queue)
+            for i in range(0,len(self.queue)):
+                if node.r<self.queue[i].r:
+                    index=i
+                    break
+        self.queue.insert(index,node)
+
+    def pop(self):
+        return self.queue.pop(0)
