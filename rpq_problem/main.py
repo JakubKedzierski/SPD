7da3260f1ff00a49104fb886333e952aa967ad40
Jakub Kedzierski <248915@student.pwr.edu.pl>
2021-04-28 18:38:42 +0200

    add testing

--- rpq_problem/main.py
+++ rpq_problem/main.py
@@ -2,6 +2,7 @@ from schrage_algorithm import *
 import numpy as np
 from piority_queue import *
 import time
+import random
 
 def read_data_set(file):
     tasks, columns = [int(x) for x in next(file).split()]
@@ -16,6 +17,24 @@ def read_data_set(file):
         p[i] = (int(row[1]))
         q[i] = (int(row[2]))
     
+    return tasks, r, p, q
+
+
+def compute_testing_set(file):
+    sets, repeats = [int(x) for x in next(file).split()]
+    for i in range(0,sets):
+        tasks, columns = [int(x) for x in next(file).split()]
+
+        r = np.zeros(tasks, dtype=int)
+        p = np.zeros(tasks, dtype=int)
+        q = np.zeros(tasks, dtype=int)
+
+        for i in range(0, tasks):
+            row = next(file).split()
+            r[i] = (int(row[0]))
+            p[i] = (int(row[1]))
+            q[i] = (int(row[2]))
+
     return tasks, r, p, q
 
 
@@ -61,5 +80,49 @@ def main():
         raise FileNotFoundError
 
 
+def testing_main():
+    sets = [50,100,150,200,250,300,400,500,1000,2000,3000]
+    repeat = 5
+
+    for numb in sets:
+        time_b = 0
+        cmax_b = 0
+
+        time_p = 0
+        cmax_p = 0
+
+        for i in range(0, repeat):
+            r = np.zeros(numb, dtype=int)
+            p = np.zeros(numb, dtype=int)
+            q = np.zeros(numb, dtype=int)
+            for j in range(0,numb):
+                r[j] = random.randint(10, 1500)
+                p[j] = random.randint(5, 50)
+                q[j] = random.randint(10, 1500)
+
+            start = time.time()
+            schedule, Cmax = basic_schrage_algorithm(numb, r, p, q)
+            end = time.time()
+            time_b = time_b + (end - start)
+            cmax_b = cmax_b + Cmax
+
+            start = time.time()
+            schedule, Cmax = basic_schrage_algorithm_priority_queue(numb, r, p, q)
+            end = time.time()
+            time_p = time_p + (end - start)
+            cmax_p = cmax_p + Cmax
+
+        time_b = time_b/repeat
+        cmax_b = cmax_b/repeat
+
+        time_p = time_p/repeat
+        cmax_p = cmax_p/repeat
+
+        print(numb)
+        print('basic:',cmax_b,time_b)
+        print('priority:', cmax_p, time_p)
+
+
+
 if __name__ == '__main__':
-    main()
+    testing_main()
